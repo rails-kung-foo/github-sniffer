089c0f96ab12abb0a13c07b31279b73530e041c8
rails-kung-foo <rails.kung.foo@gmail.com>
2016-05-28 16:32:15 +0100

    Collecting data from repo
    - New method to collect and summing up of the programming language of the repositorie.

--- lib/github_sniffer/cli/application.rb
+++ lib/github_sniffer/cli/application.rb
@@ -13,8 +13,17 @@ module GithubSniffer
       desc 'get_user USERNAME', 'Returns github profile of user'
       def get_user(username)
         github = Github.new(user: username)
+        languages = {}
 
-        p github
+        # Cyle throug repos and sum up the main language
+        github.repos.list.each do |repo|
+          repo_language = repo.language
+
+          languages[repo_language] = 0 if languages[repo_language].nil?
+          languages[repo_language] += 1
+        end
+
+        p languages
       end
 
     end # Application
