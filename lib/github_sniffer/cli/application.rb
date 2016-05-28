9eef09c3626e68690e0422d29253ac75ca6441e0
rails-kung-foo <rails.kung.foo@gmail.com>
2016-05-28 16:42:52 +0100

    improving class
    - removed not used method
    - better comment

--- lib/github_sniffer/cli/application.rb
+++ lib/github_sniffer/cli/application.rb
@@ -4,18 +4,12 @@ module GithubSniffer
   module Cli
     class Application < Thor
 
-      desc 'test NAME', 'test name'
-      def test(name)
-        greeting = "Test is fine, #{name}"
-        puts greeting
-      end
-
       desc 'get_user USERNAME', 'Returns github profile of user'
       def get_user(username)
         github = Github.new(user: username)
         languages = {}
 
-        # Cyle throug repos and sum up the main language
+        # Cycle through repos and sum up the main language
         github.repos.list.each do |repo|
           repo_language = repo.language.to_s.downcase!
 
@@ -23,6 +17,8 @@ module GithubSniffer
           languages[repo_language] += 1
         end
 
+        # The key is the language name and value has the count,
+        # we return the highest count for a langauge
         prefered_lang = languages.sort_by{ |_k, value| value }.last
 
         p "#{username} has #{prefered_lang[0]} as main language in #{prefered_lang[1]} repos."
